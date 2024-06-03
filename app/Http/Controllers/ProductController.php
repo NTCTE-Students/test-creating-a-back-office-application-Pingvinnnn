bf97f6111daaf729312e673b6f4b3f5c5377234b
Alexey Chumak <cumaka427@gmail.com>
2024-06-03 12:27:10 +0500

    controllers

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Product;
+use Orchid\Platform\Http\Controllers\Controller;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        $products = Product::all();
+        return view("products.index", compact("products"));
+    }
+
+    public function create()
+    {
+        return view("products.create");
+    }
+
+    public function store(Request $request)
+    {
+        $product = new Product();
+        $product->name = $request->input('name');
+        $product->description = $request->input('description');
+        $product->price = $request->input('price');
+        $product->quantity = $request->input('quantity');
+        $product->save();
+        return redirect()->route('products.index');
+    }
+
+    public function edit($id)
+    {
+        $product = Product::find($id);
+        return view('products.edit', compact('product'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $product = Product::find($id);
+        $product->name = $request->input('name');
+        $product->description = $request->input('description');
+        $product->price = $request->input('price');
+        $product->quantity = $request->input('quantity');
+        $product->save();
+        return redirect()->route('products.index');
+    }
+
+    public function destroy($id)
+    {
+        $product = Product::find($id);
+        $product->delete();
+        return redirect()->route('products.index');
+    }
+}
